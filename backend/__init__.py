349de2c9c316f04381af6d65de0063bcda5a0a32
Surya HA <suryahariharan2006@gmail.com>
2026-02-13 00:44:48 +0530

    Updated code base

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,4 +3,10 @@
 
 __version__ = '1.0.0'
 __author__ = 'Surya Hariharan'
-__email__ = 'your.email@example.com'
+__email__ = 'suryahariharan2006@gmail.com'
+__github__ = 'https://github.com/Surya-Hariharan'
+__linkedin__ = 'https://linkedin.com/in/surya-ha'
+
+from .app import create_app
+
+__all__ = ['create_app']
